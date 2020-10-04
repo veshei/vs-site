9cb78aae9dc22c989394f1f61710e82cc741270b
Veronica Shei <shei.v@husky.neu.edu>
2020-10-04 00:59:29 -1000

    feat: Added absolute paths

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,20 @@
  *
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
+const path = require("path")
 
 module.exports = {
   /* Your site config here */
-  plugins: [`gatsby-plugin-styled-components`, `gatsby-plugin-react-helmet`],
+  plugins: [
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: "gatsby-plugin-root-import",
+      options: {
+        src: path.join(__dirname, "src"),
+        pages: path.join(__dirname, "src/pages"),
+        fonts: path.json(__dirname, "src/fonts"),
+      },
+    },
+  ],
 }
