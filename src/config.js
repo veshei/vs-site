5eb33ba4e0477e40b25a6ae9a604a963e6a239b5
Veronica Shei <veronicacshei@gmail.com>
2020-10-04 21:31:24 -1000

    feat: Added social media handles to config data

--- src/config.js
+++ src/config.js
@@ -1,3 +1,26 @@
 module.exports = {
   email: "veronicacshei@gmail.com",
+
+  socialMedia: [
+    {
+      name: "Gmail",
+      url: "veronicacshei@gmail.com",
+    },
+    {
+      name: "Linkedin",
+      url: "https://www.linkedin.com/in/veronica-shei/",
+    },
+    {
+      name: "GitHub",
+      url: "https://github.com/veshei",
+    },
+    {
+      name: "Facebook",
+      url: "https://www.facebook.com/veshei/",
+    },
+    {
+      name: "Instagram",
+      url: "https://www.instagram.com/hi.veshei/",
+    },
+  ],
 }
